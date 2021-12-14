6b2f7ca649afd726cd3b344d093478587805cf6b
442006357 <91495712+442006357@users.noreply.github.com>
2021-12-14 12:07:21 +0300

    Create Age.java

---
+++ Age.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class  Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int Y77;
+        System.out.println("Calculate your date of birth");
+        Y77= scan.nextInt();
+        System.out.println(2021-Y77);
+
+
+    }
+}
+
+//YAZN

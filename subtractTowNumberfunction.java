cc2a166ba9ed5327bca7f5833b8379636d53ff12
442006357 <91495712+442006357@users.noreply.github.com>
2021-12-14 23:04:56 +0300

    Create subtractTowNumberfunction.java

---
+++ subtractTowNumberfunction.java
@@ -0,0 +1,17 @@
+
+public class subtractTowNumberfunction {
+  static int mymethod(int Y77 , int Z66 ) {
+    return Y77 - Z66;
+  }
+  
+ 
+  
+  public static void main(String[] args) {
+    int num = mymethod(15, 13);
+ 
+    System.out.println( num);
+    
+  }
+}
+//YAZN	
+

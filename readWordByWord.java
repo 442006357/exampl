9b018ddc7d24a00ef1e458cfcf98b032a37c4c64
442006357 <91495712+442006357@users.noreply.github.com>
2021-12-14 23:14:24 +0300

    Create  readWordByWord.java

---
+++ readWordByWord.java
@@ -0,0 +1,11 @@
+public class readWordByWord.java {
+        public static void main(String[] args) {
+                String xxx = "waleed\nalshrhri";
+
+                System.out.println(xxx);
+
+
+
+        }
+}
+//YAZN

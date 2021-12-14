36389995b1348274b78a848f4ba7fce49653f81a
442006357 <91495712+442006357@users.noreply.github.com>
2021-12-14 11:45:10 +0300

    Create Stars.java

---
+++ Stars.java
@@ -0,0 +1,19 @@
+
+public class Main {
+public static void main(String[] args) {
+        int p = 0;
+        while(p <= 5){
+        int g = 1;
+        while(g <= p){
+        System.out.print("*");
+        ++g;
+        }
+
+        System.out.println("*");
+
+        ++p;
+        }
+        }
+        }
+        
+//YAZN

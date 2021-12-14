6036a5d46400e5105e9b6cbecb6898d14d80ba24
442006357 <91495712+442006357@users.noreply.github.com>
2021-12-14 11:50:59 +0300

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int j;
+        System.out.print("Enter a Number");
+        j = scan.nextInt();
+        j %=2;
+        if (j==0){
+            System.out.print("even");
+        }
+        else {
+            System.out.print("odd");
+        }
+
+
+
+    }
+}
+
+//YAZN

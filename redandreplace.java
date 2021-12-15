d228dedca3c66453f4d1414bc67bc2652addc562
442006357 <91495712+442006357@users.noreply.github.com>
2021-12-15 16:24:22 +0300

    Create  redandreplace.java

---
+++ redandreplace.java
@@ -0,0 +1,12 @@
+
+public class redandreplace.java{
+public static void main(String[] args) {
+     String name = "YZNALKIRI";
+     for (int x =0;  x<=9 ; ++x){
+         if(name.charAt(x) == 'I'){
+        System.out.println('x');}
+        else {
+            System.out.println(name.charAt(x));
+        }
+        
+     }}}
